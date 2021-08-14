30cfdc132146f903e5b3ead99c3c5eb381641ff0
Esteban <fajardo.esteban@gmail.com>
2021-08-14 05:21:19 -0500

    config react native dotenv package

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-// import { API_URL } from "react-native-dotenv";
+import { API_URL } from "@env";
 // console.warn(API_URL);
 const api = axios.create({
-  baseURL: "https://www.reddit.com", //API_URL,
+  baseURL: API_URL,
 });
 
 export default api;
